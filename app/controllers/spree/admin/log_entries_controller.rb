57de61f8bc947bdae1687da101356f5f8876bcfb
Ryan Siddle <ryan@whitestarmedia.co.uk>
2012-09-25 09:40:35 +0100

    Move into Spree namespace

---
+++ app/controllers/spree/admin/log_entries_controller.rb
@@ -0,0 +1,19 @@
+module Spree
+  module Admin
+    class LogEntriesController < ResourceController
+    
+
+    private
+      def collection
+        return @collection if @collection
+
+        scope = LogEntry.scoped
+
+        scope = scope.where(:source_type => params[:source_type]) if params[:source_type]
+        scope = scope.where(:source_id   => params[:source_id])   if params[:source_id]
+
+        @collection = scope.page(params[:page])
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
-Rails.application.routes.draw do
-  namespace :admin do 
+Spree::Core::Engine.routes.prepend do
+
+  namespace :admin do
     resources :log_entries
   end
+  
 end
+
