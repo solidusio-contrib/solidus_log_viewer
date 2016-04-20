c31c55aae94479bace5111e187961279156abcfd
Kevin Attfield <kevin@stembolt.com>
2016-04-20 15:01:38 -0700

    Remove erroring line
    I don't know what this does, but I don't need it and it breaks things.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,5 @@ RSpec.configure do |config|
   # examples within a transaction, comment the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-end
 
-@configuration ||= AppConfiguration.find_or_create_by_name("Default configuration")
+end
