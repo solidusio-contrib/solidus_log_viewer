77649ddffb68dbdf1a959d4a94eb59d485d1791d
Elia Schito <elia@schito.me>
2020-01-24 12:37:57 +0100

    Re-generate the gem with solidus_dev_support

---
+++ lib/solidus_log_viewer/engine.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spree/core'
+require 'deface'
+
+module SolidusLogViewer
+  class Engine < Rails::Engine
+    include SolidusSupport::EngineExtensions::Decorators
+
+    isolate_namespace ::Spree
+
+    engine_name 'solidus_log_viewer'
+
+    # use rspec for tests
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+  end
+end
