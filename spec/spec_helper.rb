b778b34ae3879d39b4944aae1c6eb2965e86c99d
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-05-25 10:19:35 -0600

    Updated factories loading after recent changes in solidus
    This commit reflects changes made by Solidus [PR #169](solidusio/solidus_dev_support#169 (comment))
    When testing extensions using Solidus vesions that support
    the definition_file_paths, it will load the core factory first
    and then the ones defined in
    `lib/extension_name/testing_support/factories`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,9 @@ require 'solidus_dev_support/rspec/feature_helper'
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
-# Requires factories defined in lib/solidus_log_viewer/factories.rb
-require 'solidus_log_viewer/factories'
+# Will load Solidus core factory first and then the ones
+# defined in `lib/solidus_log_viewer/testing_support/factories`.
+SolidusDevSupport::TestingSupport::Factories.load_for(SolidusLogViewer::Engine)
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
